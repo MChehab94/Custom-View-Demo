70f74431019feacda9f6bf8ca27de806d0c3917b
Mohamad <lemirmohammad.shehab@lau.edu>
2017-12-30 19:52:41 +0200

    Removed url variable and constructor implementation

--- app/src/main/java/mchehab/com/customview/MyImageView.java
+++ app/src/main/java/mchehab/com/customview/MyImageView.java
@@ -1,7 +1,6 @@
 package mchehab.com.customview;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
@@ -14,18 +13,11 @@ import com.squareup.picasso.Picasso;
 
 public class MyImageView extends AppCompatImageView {
 
-    private String url;
-
     public MyImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable
-                .MyImageView, 0, 0);
-        url = typedArray.getString(R.styleable.MyImageView_imageURL);
-        Picasso.with(context).load(url).into(this);
     }
 
     public void setImageURL(String url){
-        this.url = url;
         Picasso.with(getContext()).load(url).into(this);
     }
 }
